64ce63976dc2effa5464e17c5e954a8bacd19374
Hoku <hokuto.u0113@gmail.com>
2023-05-24 17:27:13 +0900

    optimize main.py

--- function/model.py
+++ function/model.py
@@ -2,9 +2,7 @@ from super_gradients.training import models
 from super_gradients.common.object_names import Models
 from ultralytics import YOLO
 
-def modelLoad(args):
-    path = args
-
+def modelLoad(path):
     if '.pt' in path and '.pth' in path:
         model = models.get(Models.YOLO_NAS_S,num_classes=2, checkpoint_path=path).cuda()
         return model
@@ -12,7 +10,7 @@ def modelLoad(args):
     #elif 'trt' in path:
     
     else:
-        print("ばぁぁあか")
+        print("Only supported `.pt`, `.pth`, `.trt`")
 
 #pt pth trtを読み込むお
 #pt pth→super…
--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 import cv2
-# import torch
+import argparse
 from function.model import modelLoad
 
-def main():
-    hogemodel = modelLoad('./model/ckpt_best.pth')
-    capture = cv2.VideoCapture(1)
-    is_opened = capture.isOpened()
-    if not is_opened:
-        print("camera is not found.")
-        exit(0)
-    while is_opened:
-        is_returned, frame = capture.read()
-        if not is_returned:
-            break
+def detect():
+    # loading model
+    model = modelLoad(args.model)
 
-        hogemodel.predict(frame)
-        # TODO: 推論のための加工
-        # TODO: 推論
-        # TODO: 特定領域における散布必要性の判断
-        # TODO: 加速度センサによる散布タイミングの取得
-        # TODO: 散布モジュールへ伝達
-#----Debug----
-        cv2.imshow("web camera", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
-#-------------
-    capture.release()
+    # source is `int` type web cam
+    
+    print(args.source)
+    print(type(args.source))
+
+    if args.source == "0" or args.source == "1":
+        source = int(args.source)
+        cap = cv2.VideoCapture(source)
+        is_opened = cap.isOpened()
+
+        if not is_opened:
+            print("camera is not found.")
+            exit(0)
+
+        while cv2.waitKey(1) != 27: 
+            res, frame = cap.read()
+            model.predict(frame)
+            cv2.imshow("web camera", frame)
+        cap.release()
+        cv2.destroyAllWindows()
+    
+    elif args.source in [".jpg", "png", "svg"]:
+        image = cv2.imread(args.source)
+        pred = model.predict(image)
+        pred.save(output_folder="output/")
+    
+    elif args.source in [".mp4"]:
+        cap = cv2.VideoCapture(args.source)
+
+        while cv2.waitKey(1) != 27:
+            res, frame = cap.read()
+            model.predict(frame)
+            cv2.imshow("video", frame)
+        
+        cap.release()
+        cv2.destroyAllWindows()
+    
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", type=str, default=0) # web cam
+    parser.add_argument("--model", type=str)
+    args = parser.parse_args()
+
+    detect()
